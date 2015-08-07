2a02a9c4dbfdc121018792b41ae12b807a97c609
Kevin McNamee <mcnameekm@gmail.com>
2015-08-07 08:15:54 -0400

    Initial Commit

---
+++ trainspotting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  trainspotting
+//
+//  Created by Kevin McNamee on 8/7/15.
+//  Copyright (c) 2015 Frothy Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ trainspottingTests/trainspottingTests.m
@@ -0,0 +1,40 @@
+//
+//  trainspottingTests.m
+//  trainspottingTests
+//
+//  Created by Kevin McNamee on 8/7/15.
+//  Copyright (c) 2015 Frothy Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface trainspottingTests : XCTestCase
+
+@end
+
+@implementation trainspottingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
